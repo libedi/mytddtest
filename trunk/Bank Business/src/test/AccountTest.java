46f9ad2664eff372c293c3335c17e01ec3919a88
libedi@gmail.com <libedi@gmail.com@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-02-21 07:28:39 +0000

    JUnit을 사용하지 않은 TDD 첫번째 예제

--- trunk/Bank Business/src/test/AccountTest.java
+++ trunk/Bank Business/src/test/AccountTest.java
@@ -1,11 +1,24 @@
 package test;
 
+import main.Account;
+
 public class AccountTest {
 	
-	/*
-	 * My First TDD Test
-	 */
-	public void testAccount(){
+	public void testAccount() throws Exception{
 		Account account = new Account();
+		if( account != null ){
+			throw new Exception("계좌생성 실패!");
+		}
+	}
+	
+	public static void main(String args[]){
+		AccountTest test = new AccountTest();
+		try {
+			test.testAccount();		// 테스트 케이스 실행
+		} catch (Exception e) {
+			System.out.println("실패(X)");		// 예외가 발생하면 실패(X)
+			return;
+		}
+		System.out.println("성공(O)");
 	}
 }
