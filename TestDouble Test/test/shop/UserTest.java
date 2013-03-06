b752a200a55c3ec51038f514ec31ca70e2ef3c11
libedi@gmail.com <libedi@gmail.com@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-03-06 05:21:15 +0000

    Test Spy 사용, Test Double을 익명클래스로 사용

--- TestDouble Test/test/shop/UserTest.java
+++ TestDouble Test/test/shop/UserTest.java
@@ -30,7 +30,7 @@ public class UserTest {
 	@Test
 	public void testGetOrderPrice_discountableItem() throws Exception {
 		PriceCalculator calculator = new PriceCalculator();
-		// new Item(�̸�, ī�װ?, ����)
+		// new Item(이름, 카테고리, 가격)
 		Item item = new Item("LightSavor", "부엌칼", 100000);
 		ICoupon coupon = new StubCoupon();
 		
@@ -52,6 +52,63 @@ public class UserTest {
 		ICoupon otherCoupon = new FakeCoupon();
 		
 		assertEquals("쿠폰 적용 안되는 아이템", 20000, calculator.getOrderPrice(item, otherCoupon));
+	}
+	
+	@Test
+	public void testGetOrderPrice_discountableItem_usingTestSpy() throws Exception {
+		PriceCalculator calculator = new PriceCalculator();
+		// new Item(이름, 카테고리, 가격)
+		Item item = new Item("LightSavor", "부엌칼", 100000);
+		
+		ICoupon coupon = new SpyCoupon();
+		assertEquals("쿠폰으로 인해 할인된 가격", 93000, calculator.getOrderPrice(item, coupon));
+		
+		int methodCallCount = ((SpyCoupon)coupon).getIsAppliableCallCount();
+		assertEquals("coupon.isAppliable 메서드 호출 횟수", 1, methodCallCount);
+	}
+	
+	@Test
+	public void testAddCoupon_usingAnonymousClass() throws Exception {
+		User user = new User("libedi");
+		assertEquals("최초 쿠폰 수", 0, user.getTotalCouponCount());
+		
+		// 공통적으로 계속 사용될 경우, 필요하다면 coupon을 필드로 뽑아내고, 익명 클래스 부분을 setUp() 메서드로 이동시켜도 된다.
+		ICoupon coupon = new ICoupon() {
+
+			@Override
+			public String getName() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public boolean isValid() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public int getDiscountPercent() {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public boolean isAppliable(Item item) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public void doExpire() {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		};
+		
+		user.addCoupon(coupon);
+		assertEquals("쿠폰 수령 후", 1, user.getTotalCouponCount());
 	}
 	
 }
