5b01b0a06cdc1b7ef6b8a2ab920221a6ce7ef70d
libedi@gmail.com <libedi@gmail.com@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-03-12 08:59:04 +0000

    Unitils 업로드

---
+++ 5_TDDWithUnitils/test/unitils/Book.java
@@ -0,0 +1,30 @@
+package unitils;
+
+public class Book {
+
+	private String name;
+	private String author;
+	private int price;
+	
+	public Book(String name, String author, int price) {
+		this.name = name;
+		this.author = author;
+		this.price = price;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return this.author;
+	}
+
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return this.price;
+	}
+
+}
