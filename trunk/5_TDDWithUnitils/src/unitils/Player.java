8f28ab978280f361c05c9e3d381a3faeee87c176
libedi@gmail.com <libedi@gmail.com@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-03-13 07:33:29 +0000

    Unitils 를 사용한 TDD

---
+++ trunk/5_TDDWithUnitils/src/unitils/Player.java
@@ -0,0 +1,15 @@
+package unitils;
+
+public class Player {
+	
+	private String name;
+	private int age;
+	private int experienceYear;
+
+	public Player(String name, int age, int experienceYear) {
+		this.name = name;
+		this.age = age;
+		this.experienceYear = experienceYear;
+	}
+
+}
