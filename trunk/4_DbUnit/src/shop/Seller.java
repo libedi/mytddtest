368ba39e695a07f612f772b4ec7b365a35043456
libedi@gmail.com <libedi@gmail.com@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-03-13 08:45:36 +0000

    DbUnit 업로드

---
+++ trunk/4_DbUnit/src/shop/Seller.java
@@ -0,0 +1,30 @@
+package shop;
+
+public class Seller {
+
+	private String id;
+	private String name;
+	private String email;
+	
+	public Seller(String id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+	}
+
+	public String getId() {
+		// TODO Auto-generated method stub
+		return this.id;
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return this.name;
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return this.email;
+	}
+
+}
---
+++ trunk/4_DbUnit/test/shop/DatabaseRepository.java
@@ -0,0 +1,108 @@
+package shop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseRepository implements Repository {
+	
+	private final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	private final String protocol = "jdbc:derby:";
+	private final String dbName = "shopdb";
+	private Connection conn;
+	
+	public DatabaseRepository() throws Exception {
+		Class.forName(driver).newInstance();
+		conn = DriverManager.getConnection(protocol + dbName); 
+	}
+
+	@Override
+	public Seller findById(String id) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		Seller seller = null;
+		
+		try {
+			String query = "select id, name, email from seller where id=?";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if ( ! rs.next() ) {
+				throw new SQLException("No Data Fount!");
+			}
+			seller = new Seller(rs.getString(1), rs.getString(2), rs.getString(3));
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return seller;
+	}
+
+	@Override
+	public void add(Seller seller) {
+		PreparedStatement pstmt = null;		
+		try {
+			String query = "insert into seller values(?, ?, ?)";
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, seller.getId());
+			pstmt.setString(2, seller.getName());
+			pstmt.setString(3, seller.getEmail());
+			int result = pstmt.executeUpdate();
+			if( result == 0 ){
+				throw new SQLException("Data was not inserted!");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if( pstmt != null ) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void update(Seller seller) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void remove(Seller seller) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
