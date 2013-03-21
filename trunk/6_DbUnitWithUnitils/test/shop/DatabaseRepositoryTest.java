2459f1fb8ce303780d2cec33f0ec89a0d43e5559
libedi <libedi@9c916233-4cab-a4b9-1976-3a84a620882a>
2013-03-21 01:35:09 +0000

    unitils 수정

--- trunk/6_DbUnitWithUnitils/test/shop/DatabaseRepositoryTest.java
+++ trunk/6_DbUnitWithUnitils/test/shop/DatabaseRepositoryTest.java
@@ -2,10 +2,12 @@ package shop;
 
 import static org.unitils.reflectionassert.ReflectionAssert.assertPropertyLenientEquals;
 
+import org.dbunit.database.IDatabaseConnection;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.unitils.UnitilsJUnit4TestClassRunner;
 import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.dbunit.annotation.ExpectedDataSet;
 
 @RunWith(UnitilsJUnit4TestClassRunner.class)		
 @DataSet							
@@ -19,6 +21,18 @@ public class DatabaseRepositoryTest {
 		assertPropertyLenientEquals("id", "horichoi", actualSeller);
 		assertPropertyLenientEquals("name", "sh.choi", actualSeller);
 		assertPropertyLenientEquals("email", "megaseller@hatmail.com", actualSeller);
+	}
+	
+	/*
+	 * 예상 데이터셋을 이용한 테스트 메서드 레벨의 결과 비교.
+	 * 예상 데이터셋(expected_seller.xml) 과 결과를 비교한다.
+	 */
+	@Test
+	@ExpectedDataSet("expected_seller.xml")
+	public void testAddNewSeller() throws Exception {
+		Seller newSeller = new Seller("hssm", "dw.lee", "scala@hssm.kr");
+		Repository repository = new DatabaseRepository();
+		repository.add(newSeller);
 	}
 	
 }
